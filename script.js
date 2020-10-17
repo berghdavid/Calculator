5391b7805f45c22e6e2effab467e8b3deb1cbe98
David <berghdavid@hotmail.com>
2020-10-17 04:23:48 +0200

    operator?

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const a = "aight";
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (operator, a, b) {
+    operator(a, b);
+}
